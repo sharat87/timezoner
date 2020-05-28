f0cb23413a6d204a9502cebafd9d175732496e44
Shrikant Sharat Kandula <shrikantsharat.k@gmail.com>
2020-05-28 12:49:36 +0530

    Push as gh-pages in deploy script.

--- deploy.sh
+++ deploy.sh
@@ -13,3 +13,12 @@ npm run build
 find . -depth 1 ! -name dist -exec rm -rf '{}' +
 find dist -depth 1 -exec mv '{}' . ';'
 rmdir dist
+
+git add .
+git commit -m "Auto-build"
+git remote set-url origin git@github.com:sharat87/timezoner.git
+git push --force origin gh-pages
+
+popd
+
+rm -rf "$tmp_dir"
